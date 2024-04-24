d5afa64c50dfaa742ea9027084c2507a52f3d02a
mamba <isaiahntina47@gmail.com>
2024-04-24 06:11:28 +0000

    Refactor image processing functionality using Sharp library

--- utils/imageProcessor.ts
+++ utils/imageProcessor.ts
@@ -1,6 +1,7 @@
 import * as tf from '@tensorflow/tfjs-node';
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import { Readable } from 'stream';
+import sharp from 'sharp';
 
 /**
  * Processes an image by reading it from a stream, loading a model, and making predictions.
@@ -16,13 +17,13 @@ export async function processImage(imageStream: Readable) {
     }
     console.log('Received all chunks');
     const imageBuffer = Buffer.concat(chunks);
+    const pngBuffer = await sharp(imageBuffer).png().toBuffer();
 
-    // Load the Coco SSD model
     console.log('Loading model...');
     const model = await cocoSsd.load();
 
     // Decode the image into a tensor because Coco SSD expects a tensor as input
-    const tensor = tf.node.decodeImage(imageBuffer).toFloat().expandDims(0) as tf.Tensor4D;
+    const tensor = tf.node.decodeImage(pngBuffer).toFloat().expandDims(0) as tf.Tensor4D;
 
     // Resize the image to 300x300
     const resizedTensor = tf.image.resizeBilinear(tensor, [300, 300]);
