d43bf905da28f02f44fd25162b36b491a3dc2d6a
mamba <isaiahntina47@gmail.com>
2024-04-24 03:09:46 +0000

    Refactor file upload API endpoint and add image processing

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,19 +1,38 @@
-// receive file from client and save it to the server
+// path: /api/upload.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+import axios from 'axios';
+import { processImage } from '../../utils/imageProcessor';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        console.log('Received file');
-        res.status(200).json({ message: 'File received' });
+        const { imgUr } = req.query;
+        if (!imgUr) {
+            console.error('No problem for now we are going to use the default image');
+        }
+        const imgUrl = 'https://t4.ftcdn.net/jpg/00/65/70/65/360_F_65706597_uNm2SwlPIuNUDuMwo6stBd81e25Y8K8s.jpg';
+        if (!imgUrl) {
+            return res.status(400).json({ error: 'Image URL is required' });
+        }
+
+        try {
+            // Download the image from the provided URL
+            const response = await axios.get(imgUrl as string, { responseType: 'stream' });
+            if (response.status !== 200) {
+                return res.status(400).json({ error: 'Failed to download image' });
+            }
+
+            // Process the image and detect objects
+            // const model = await cocoSsd.load();
+            const predictions = await processImage(response.data);
+
+            // Respond back with detected objects
+            res.status(200).json({ message: 'Image processed successfully', predictions });
+        } catch (error) {
+            console.error('Error processing image:', error);
+            res.status(500).json({ error: 'Error processing the image' });
+        }
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
-
