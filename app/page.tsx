69ff621d4557c23918d21f7134588c2a58ab6582
mamba <isaiahntina47@gmail.com>
2024-04-24 00:58:06 +0000

    Add page component for Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ImageUploadComponent from '../components/image-upload';
+
+export default function Home() {
+    return (
+        <div>
+            <ImageUploadComponent />
+        </div>
+    );
+}
