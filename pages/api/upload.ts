e9c1e0e848ba941509dffd27b0649b92a12ea1e2
mamba <isaiahntina47@gmail.com>
2024-04-24 06:11:35 +0000

    Refactor image upload and processing functionality, and integrate with Sharp library

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,27 +1,25 @@
-// path: /api/upload.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import { processImage } from '../../utils/imageProcessor';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const imgUrl = req.body;
+        const { imgUrl } = req.body; // Extract imgUrl from the request body
         if (!imgUrl) {
             return res.status(400).json({ error: 'Image URL is required' });
         }
         console.log('Received image:', imgUrl);
-
         try {
             // Download the image from the provided URL
-            const response = await axios.get(imgUrl as string, { responseType: 'stream' });
+            const response = await axios.get(imgUrl, { responseType: 'stream' });
             if (response.status !== 200) {
                 return res.status(400).json({ error: 'Failed to download image' });
             }
             
-            const predictions = await processImage(response.data);
+            const model_predictions = await processImage(response.data);
 
             // Respond back with detected objects
-            res.status(200).json({ message: 'Image processed successfully', predictions });
+            res.status(200).json({ message: 'Image processed successfully', model_predictions });
         } catch (error) {
             console.error('Error processing image:', error);
             res.status(500).json({ error: 'Error processing the image' });
